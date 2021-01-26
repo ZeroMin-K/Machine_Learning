b2fa4f4736544247df9cae30399312e107d3d000
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-26 22:12:06 +0900

    Create numpy_matrix.py

---
+++ basic_concepts/numpy_matrix.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+A = np.array([[1, 2, 3], [4, 5, 6]])
+B = np.array([[-1, -2, -3], [-4, -5, -6]])
+
+# matrix A, B 형상 출력
+print('A.shape ==', A.shape, ', B.shape ==', B.shape)
+
+# matrix A, B 차원 출력
+print('A.ndim ==', A.ndim, ', B.ndim ==', B.ndim)
