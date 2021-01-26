9043220cf518dacac2ca6420adcbc845bb1b8154
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-26 22:10:18 +0900

    Create numpy_vector.py

---
+++ basic_concepts/numpy_vector.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+A = np.array([1, 2, 3])
+B = np.array([4, 5, 6])
+
+# vector A, B 출력
+print('A ==', A, ', B ==', B)
+
+# vector A, B 형상 출력
+print('A.shape ==', A.shape, ', B.shape ==', B.shape)
+
+# vecotr A, B 차원 출력
+print('A.ndim ==', A.ndim, ', B.ndim ==', B.ndim)
