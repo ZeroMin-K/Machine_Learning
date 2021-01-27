7034385c5aa1be203157b27e9007eae8c81cb560
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-27 15:51:47 +0900

    Update numerical_derivative_basic.py

--- basic_concepts/numerical_derivative_basic.py
+++ basic_concepts/numerical_derivative_basic.py
@@ -1,10 +1,8 @@
-from math import e
-
 def my_func1(x):
     return x ** 2
 
 def numerical_derivative(f, x):
-    delta_x = 1 * e -4
+    delta_x = 1e-4
     return (f(x+delta_x) - f(x-delta_x)) / (2*delta_x)
 
 result = numerical_derivative(my_func1, 3)
